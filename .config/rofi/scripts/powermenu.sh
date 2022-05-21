8c6e4f2dc9ec283936e5662abbd3b182c988cbe1
Agustin Grillo <agrillo@itba.edu.ar>
2022-05-21 14:35:42 -0300

    Change rofi power menu

--- .config/rofi/scripts/powermenu.sh
+++ .config/rofi/scripts/powermenu.sh
@@ -15,7 +15,7 @@
 # full_circle     full_square     full_rounded     full_alt
 # row_circle      row_square      row_rounded      row_alt
 
-theme="card_rounded"
+theme="powermenu"
 dir="$HOME/.config/rofi/themes"
 
 # random colors
