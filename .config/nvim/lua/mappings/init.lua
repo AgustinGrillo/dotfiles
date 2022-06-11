5411a5da78c5d07513290d33a0b1fe76485089f1
Agustin Grillo <agrillo@itba.edu.ar>
2022-06-10 21:31:17 -0300

    Cange neovim comment keybinding

--- .config/nvim/lua/mappings/init.lua
+++ .config/nvim/lua/mappings/init.lua
@@ -12,8 +12,8 @@ map('n', '<leader>fw', ':Telescope live_grep <CR>', opts)
 map('n', '<leader>fc', ':Telescope grep_string <CR>', opts)
 map('n', '<leader>fy', ':Telescope neoclip <CR>', opts)
 -- Commenting
-map('n', '<C-Bslash>', "<Plug>(comment_toggle_current_linewise)", opts)
-vim.keymap.set('x', '<C-Bslash>', '<Plug>(comment_toggle_blockwise_visual)')
+vim.keymap.set('n', '<C-Bslash>', "v:count == 0 ? '<Plug>(comment_toggle_current_linewise)' : '<Plug>(comment_toggle_linewise_count)'", { expr = true, remap = true })
+vim.keymap.set('x', '<C-Bslash>', '<Plug>(comment_toggle_linewise_visual)')
 -- Window Navigation
 map('n', '<A-w>,', ':wincmd h <CR>', opts)
 map('n', '<A-w><Left>', ':wincmd h <CR>', opts)
