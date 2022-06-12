994605266dde9b7cbf2d92c224580556ea00e765
Agustin Grillo <agrillo@itba.edu.ar>
2022-06-12 12:52:42 -0300

    fix lua conig indentation

--- .config/nvim/lua/lsp-config/language-servers.lua
+++ .config/nvim/lua/lsp-config/language-servers.lua
@@ -2,7 +2,7 @@ local servers = { 'pyright', 'clangd', 'sumneko_lua' , 'arduino_language_server'
 
 -- Lsp installer (Beofre lspconfig setup)
 require("nvim-lsp-installer").setup {
-  ensure_installed = servers,
+    ensure_installed = servers,
 }
 
 -- Mappings.
@@ -16,11 +16,11 @@ vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 -- LSP diagnostic config
 -- Change severity for signs and text.
 vim.diagnostic.config({
-  underline = {severity = vim.diagnostic.severity.ERROR},
-  virtual_text = {severity = {min=vim.diagnostic.severity.ERROR},
-                  spacing = 4,
-                  prefix = '●',},
-  signs = {severity = {min=vim.diagnostic.severity.WARN}},
+    underline = {severity = vim.diagnostic.severity.ERROR},
+    virtual_text = {severity = {min=vim.diagnostic.severity.ERROR},
+        spacing = 4,
+        prefix = '●',},
+    signs = {severity = {min=vim.diagnostic.severity.WARN}},
 })
 -- Add floating window to display diagnostics
 vim.o.updatetime = 500
@@ -41,28 +41,28 @@ vim.cmd [[
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+    -- Enable completion triggered by <c-x><c-o>
+    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+    -- Mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local bufopts = { noremap=true, silent=true, buffer=bufnr }
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+    vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
 
-  -- Configure Highlighting
-  require 'illuminate'.on_attach(client)
-  vim.api.nvim_command('let g:Illuminate_delay = 500')
-  vim.api.nvim_set_keymap('n', '<a-n>', '<cmd>lua require"illuminate".next_reference{wrap=true}<cr>', {noremap=true})
-  vim.api.nvim_set_keymap('n', '<a-p>', '<cmd>lua require"illuminate".next_reference{reverse=true,wrap=true}<cr>', {noremap=true})
-  vim.api.nvim_set_keymap('n', '<a-o>', '<cmd>lua require"illuminate".toggle_pause()<cr>', {noremap=true})
+    -- Configure Highlighting
+    require 'illuminate'.on_attach(client)
+    vim.api.nvim_command('let g:Illuminate_delay = 500')
+    vim.api.nvim_set_keymap('n', '<a-n>', '<cmd>lua require"illuminate".next_reference{wrap=true}<cr>', {noremap=true})
+    vim.api.nvim_set_keymap('n', '<a-p>', '<cmd>lua require"illuminate".next_reference{reverse=true,wrap=true}<cr>', {noremap=true})
+    vim.api.nvim_set_keymap('n', '<a-o>', '<cmd>lua require"illuminate".toggle_pause()<cr>', {noremap=true})
 end
 
 
@@ -73,41 +73,41 @@ capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
 for _, lsp in pairs(servers) do
-  if lsp == 'arduino_language_server' then
-      require('lspconfig')[lsp].setup {
-        on_attach = on_attach,
-        capabilities = capabilities,
-        flags = {
-          -- This will be the default in neovim 0.7+
-          debounce_text_changes = 150,
-        },
-        cmd = {
-            "arduino-language-server",
-            "-cli-config", "/home/agus/.arduino15/arduino-cli.yaml",
-            "-fqbn", "esp32:esp32:nodemcu-32s",
-            '-cli', '/home/agus/.local/bin/arduino-cli',
-            '-clangd', '/usr/bin/clangd',
+    if lsp == 'arduino_language_server' then
+        require('lspconfig')[lsp].setup {
+            on_attach = on_attach,
+            capabilities = capabilities,
+            flags = {
+                -- This will be the default in neovim 0.7+
+                debounce_text_changes = 150,
+            },
+            cmd = {
+                "arduino-language-server",
+                "-cli-config", "/home/agus/.arduino15/arduino-cli.yaml",
+                "-fqbn", "esp32:esp32:nodemcu-32s",
+                '-cli', '/home/agus/.local/bin/arduino-cli',
+                '-clangd', '/usr/bin/clangd',
+            }
         }
-      }
-  elseif lsp == 'clangd' then
-    require('lspconfig')[lsp].setup {
-        on_attach = on_attach,
-        capabilities = capabilities,
-        flags = {
-          -- This will be the default in neovim 0.7+
-          debounce_text_changes = 150,
-        },
-        filetype = {"c", "cpp", "objc", "objcpp", "arduino", "ino"},
-      }
+    elseif lsp == 'clangd' then
+        require('lspconfig')[lsp].setup {
+            on_attach = on_attach,
+            capabilities = capabilities,
+            flags = {
+                -- This will be the default in neovim 0.7+
+                debounce_text_changes = 150,
+            },
+            filetype = {"c", "cpp", "objc", "objcpp", "arduino", "ino"},
+        }
 
-  else
-      require('lspconfig')[lsp].setup {
-        on_attach = on_attach,
-        capabilities = capabilities,
-        flags = {
-          -- This will be the default in neovim 0.7+
-          debounce_text_changes = 150,
+    else
+        require('lspconfig')[lsp].setup {
+            on_attach = on_attach,
+            capabilities = capabilities,
+            flags = {
+                -- This will be the default in neovim 0.7+
+                debounce_text_changes = 150,
+            }
         }
-      }
-  end
+    end
 end
