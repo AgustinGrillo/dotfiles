4d3c252935c02045752c0801a8d6a077b762647a
Agustin Grillo <agrillo@itba.edu.ar>
2022-06-13 16:36:19 -0300

    remove nvim tree tab

--- .config/nvim/lua/barbar-config/init.lua
+++ .config/nvim/lua/barbar-config/init.lua
@@ -1,13 +1,14 @@
-local nvim_tree_events = require('nvim-tree.events')
-local bufferline_state = require('bufferline.state')
-
-nvim_tree_events.on_tree_open(function ()
-  bufferline_state.set_offset(31, "File Tree")
-end)
-
-nvim_tree_events.on_tree_close(function ()
-  bufferline_state.set_offset(0)
-end)
+-- -- Nvim tree config
+-- local nvim_tree_events = require('nvim-tree.events')
+-- local bufferline_state = require('bufferline.state')
+--
+-- nvim_tree_events.on_tree_open(function ()
+--   bufferline_state.set_offset(31, "File Tree")
+-- end)
+--
+-- nvim_tree_events.on_tree_close(function ()
+--   bufferline_state.set_offset(0)
+-- end)
 
 -- Set barbar's options
 vim.g.bufferline = {
