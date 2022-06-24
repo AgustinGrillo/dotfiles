b0f61fbfa3ea5deeea0f5447dcfb93049f0b89c1
Agustin Grillo <agrillo@itba.edu.ar>
2022-06-24 19:08:33 -0300

    Add neovim greeter

--- .config/nvim/lua/packer-config/init.lua
+++ .config/nvim/lua/packer-config/init.lua
@@ -66,4 +66,6 @@ return require('packer').startup(function()
   use 'folke/todo-comments.nvim'
   -- Markdown plugin
   use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
+  -- Greeter
+  use 'goolord/alpha-nvim'
 end)
--- .config/nvim/plugin/packer_compiled.lua
+++ .config/nvim/plugin/packer_compiled.lua
@@ -80,6 +80,11 @@ _G.packer_plugins = {
     path = "/home/agus/.local/share/nvim/site/pack/packer/start/LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
+  ["alpha-nvim"] = {
+    loaded = true,
+    path = "/home/agus/.local/share/nvim/site/pack/packer/start/alpha-nvim",
+    url = "https://github.com/goolord/alpha-nvim"
+  },
   ["barbar.nvim"] = {
     loaded = true,
     path = "/home/agus/.local/share/nvim/site/pack/packer/start/barbar.nvim",
