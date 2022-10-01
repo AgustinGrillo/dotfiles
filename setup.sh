2cfce243d3bb57175a9bec1b5c4c3b64057b063f
AgustinGrillo <agrillo@itba.edu.ar>
2022-10-01 10:22:23 -0300

    remove unnecessary commands from setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,6 @@
 # Create softlinks to dotfiles.
 ln -sf ~/Documents/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/Documents/dotfiles/.bashrc ~/.bashrc
-ln -sf ~/Documents/dotfiles/.bash_aliases  ~/.bash_aliases 
-ln -sf ~/Documents/dotfiles/.tmux_ver_under_2.9.conf ~/.tmux.conf
+ln -sf ~/Documents/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/Documents/dotfiles/.Xresources ~/.Xresources
 rm -r ~/.vim
 ln -sfd ~/Documents/dotfiles/.vim ~/.vim 
@@ -14,7 +12,6 @@ ln -sf ~/Documents/dotfiles/.local/bin/rofi-bluetooth ~/.local/bin/rofi-bluetoot
 ln -sfd ~/Documents/dotfiles/.local/bin/scripts ~/.local/bin/scripts 
 ln -sfd ~/Documents/dotfiles/.local/share/fonts ~/.local/share/fonts
 ln -sfd ~/Documents/dotfiles/.local/share/gedit ~/.local/share/gedit
-mkdir ~/.icons 
 ln -sfd ~/Documents/dotfiles/.icons  ~/.icons  
 tar -xf ~/.icons/cherry.tar.gz -C ~/.icons
 rm -r ~/.config/bat ~/.config/dunst ~/.config/kitty ~/.config/neofetch ~/.config/networkmanager-dmenu ~/.config/nitrogen ~/.config/openbox ~/.config/redshift ~/.config/rofi ~/.config/tint2 ~/.config/zathura 
