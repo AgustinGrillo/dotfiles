f085477a1f57ec01226ebcf31a46ab42327c466d
Agustin Grillo <grilloagus@gmail.com>
2023-01-06 02:09:08 -0300

    update openbox menu to add capability to change light mode

--- .local/bin/scripts/set_color_theme.sh
+++ .local/bin/scripts/set_color_theme.sh
@@ -1,23 +1,24 @@
+#!/usr/bin/env bash
+
 # Script to set theme.
 
-# TODO: Receive theme as argument
 # TODO: Add default argument
 theme="$1"  # dark or light
-# theme="light"  # dark or light
-# theme="dark"  # dark or light
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Change kitty config
-new_conf_path="./../../../.config/kitty/kitty.${theme}.conf"
-old_conf_path="./../../../.config/kitty/kitty.conf"
+new_conf_path="${SCRIPT_DIR}/../../../.config/kitty/kitty.${theme}.conf"
+old_conf_path="${SCRIPT_DIR}/../../../.config/kitty/kitty.conf"
 cp "$new_conf_path" "$old_conf_path"
 
 # Change neovim config
-cp "./../../../.config/nvim/lua/colorschemes-config/catppuccin.${theme}.lua" ./../../../.config/nvim/lua/colorschemes-config/catppuccin.lua
+cp "${SCRIPT_DIR}/../../../.config/nvim/lua/colorschemes-config/catppuccin.${theme}.lua" "${SCRIPT_DIR}/../../../.config/nvim/lua/colorschemes-config/catppuccin.lua"
 
 # Change wallpaper
-nitrogen --set-zoom-fill "./../../../walls/${theme}.jpg"
+nitrogen --set-zoom-fill "${SCRIPT_DIR}/../../../walls/${theme}.jpg"
 
 # Change gtk theme
-cp "./../../../.config/gtk-3.0/settings.${theme}.ini" ./../../../.config/gtk-3.0/settings.ini
+cp "${SCRIPT_DIR}/../../../.config/gtk-3.0/settings.${theme}.ini" "${SCRIPT_DIR}/../../../.config/gtk-3.0/settings.ini"
 
 # TODO: Change global colors
